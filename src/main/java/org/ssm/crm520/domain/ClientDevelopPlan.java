7560cae812a7540b0f46cedf7763c666bdfb50a6
lican <295198088@qq.com>
2015-07-08 00:26:49 +0800

    modify

--- src/main/java/org/ssm/crm520/domain/ClientDevelopPlan.java
+++ src/main/java/org/ssm/crm520/domain/ClientDevelopPlan.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class ClientDevelopPlan extends IdEntity {
 
+	
 	private static final long serialVersionUID = 1L;
 	private String sn;
 	private Date planTime;
